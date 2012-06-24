ae8e1e66717bd3b09345641a91543f5e29f56677
Stefan <stefan@terminal21.de>
2012-06-24 18:44:30 +0200

    extra file for route handling

--- a2r_index.js
+++ a2r_index.js
@@ -2,62 +2,17 @@
 /* A2R Index Server
 */
 
-var mongoose = require('mongoose') ;
 var express = require('express');
-var Schema = mongoose.Schema ;
 
 var syslog = require('./lib/syslog').getInstance();
 var config = require('./lib/configloader').load('index.config');
-
-var sessions = mongoose.model('Sessions', require('./models/models.js').Sessions) ;
-
-//ToDo: read settings from config
-mongoose.connect('mongodb://localhost/a2r_index') ;
 
 a2r_index = express.createServer();
 a2r_index.set('view engine', 'jade');
 a2r_index.use(express["static"](__dirname + '/public'));
 a2r_index.use(express.bodyParser()) ;
 
-a2r_index.post('/', function(req, res) {
-  var data = req.body ;
-  var session = new sessions(data) ;
-  session.save(function (err) {
-    if (err) {
-      return res.json("bad request", 400) ;
-    } else {
-      return res.json({token: session.token}) ;
-    }
-  }) ;
-}) ;
-
-a2r_index.get('/show.:format?', function(req, res) {
-  sessions.find({}, function(err, docs) {
-    if (err) {
-      return syslog.log(syslog.LOG_ERROR, err) ;
-    } else {
-      if (req.params.format == "json") {
-        return res.json(docs) ;
-      } else {
-        return res.render('index', { sessions: docs}) ;
-      }
-    }
-  }) ;
-});
-
-a2r_index.get('/show/:id.:format?', function(req, res) {
-  sessions.findById(req.params.id, function(err, doc) {
-    if (err) {
-      return syslog.log(syslog.LOG_ERROR, err) ;
-    } else {
-      if (req.params.format == "json") {
-        return res.json(doc) ;
-      } else {
-        return res.render('show', { session: doc }) ;
-      }
-    }
-  }) ;
-}) ;
+require('./lib/routes').setRoutes(a2r_index) ;
 
 a2r_index.listen(config['index_web_port']);
 
